db31c20685d676f4d65b464ccbf775f85749e7d2
OlegBEZb <olegolego@mail.ru>
2019-04-07 23:06:27 +0300

    Лаба готова ко сдаче
    Пробую избавиться от костыля с граничным значением

--- Litvinov_Oleg/СТЗ/Lab1. Term2/circle_finder.py
+++ Litvinov_Oleg/СТЗ/Lab1. Term2/circle_finder.py
@@ -1,35 +1,44 @@
 import cv2
 import numpy as np
-import sys
-
+from scipy.spatial import distance as dist
+from sklearn.metrics.pairwise import cosine_similarity
 
 def mean_circle(img, center, radius, i):
+    # create mask to deal only with this circle
     mask = np.zeros((img.shape[0], img.shape[1], img.shape[2]), dtype=np.uint8)
     cv2.circle(mask, center, radius, color=(1, 1, 1), thickness=-1)
 
+    # convert image to hsv and multiply by mask
     hsv_img = np.zeros((img.shape[0], img.shape[1], img.shape[2]), dtype=np.uint8)
-
     cv2.cvtColor(img, code=cv2.COLOR_BGR2HSV, dst=hsv_img)
-    hsv_img = hsv_img*mask
-    cv2.imshow(str(i), hsv_img)
-    print('shape', str(len(mask[:, :, 0].nonzero()[0])))
+    hsv_img = hsv_img * mask
+    cv2.imshow('hsv mask_' + str(i), hsv_img)
+    
+    fig_area = len(mask[:, :, 0].nonzero()[0])
+    #print('area ' + str(i) + ' in pixels: ', fig_area)
+    hue_pixels_sum = np.sum(hsv_img[:, :, 1])
+    #sat_pixels_sum = np.sum(hsv_img[:, :, 1])
+    
+    mean_hue = hue_pixels_sum / fig_area
+    #mean_sat = sat_pixels_sum / fig_area
+    
+    return mean_hue#, mean_sat
 
-    return np.sum(hsv_img[:, :, 0]) / len(mask[:, :, 0].nonzero()[0])
-
-
+# load samples for comparison
 cuzn = cv2.imread('CuZn.png')
 ni = cv2.imread('Ni.jpg')
 cv2.imshow('CuZn', cuzn)
 cv2.imshow('Ni', ni)
 
+# read original image with money
 money = cv2.imread('money.jpg')
-cv2.imshow('money_BGR', money)
+#cv2.imshow('money_BGR', money)
 gray_money = cv2.cvtColor(money, cv2.COLOR_BGR2GRAY)
-cv2.namedWindow('main')
+#cv2.namedWindow('main')
 
 gray_money = cv2.GaussianBlur(gray_money, (5, 5), 0)
 gray_money = cv2.GaussianBlur(gray_money, (5, 5), 0)
-cv2.imshow('money_gray', gray_money)
+#cv2.imshow('money_gray', gray_money)
 
 circle_image = money.copy()
 
@@ -40,31 +49,34 @@ param2 = 61
 minR = 3
 maxR = 0
 
+# find money borders
 can = cv2.Canny(gray_money, param1, param2)
 cv2.imshow('Canny', can)
 
-cv2.waitKey(3)
+cv2.waitKey(0)
+
+#  Each vector is encoded as a 3-element floating-point vector (x,y,radius)
 circles = cv2.HoughCircles(gray_money, cv2.HOUGH_GRADIENT,
                            dp=dp/10, minDist=minDist,
                            param1=param1, param2=param2,
                            minRadius=minR,
                            maxRadius=maxR)
+print('circles shape: ', circles.shape, '\n')
 circles = np.uint16(np.around(circles))
 
-mean_cuzn = mean_circle(cuzn, (cuzn.shape[0]//2, cuzn.shape[1]//2), 20, 12)
-mean_ni = mean_circle(ni, (ni.shape[0]//2, ni.shape[1]//2), 20, 21) - 10
-print('cuzn: ', mean_cuzn, 'ni: ', mean_ni)
+mean_cuzn = mean_circle(cuzn, (cuzn.shape[0]//2, cuzn.shape[1]//2), cuzn.shape[0]//2, 'CuZn') + 100
+mean_ni = mean_circle(ni, (ni.shape[0]//2, ni.shape[1]//2), ni.shape[0]//2, 'Ni')
+print('mean_ni', mean_ni)
+print('mean_cuzn', mean_cuzn)
 
-num = 0
-for i in circles[0, :]:
-    mean = mean_circle(money, (i[0], i[1]), i[2] - 5, num)
-    print(num, mean)
-    if abs(mean_ni - mean) < abs(mean_cuzn - mean):
-        cv2.circle(money, (i[0], i[1]), i[2], (0, 255, 0), 2)
+for i, circle in enumerate(circles[0, :]):
+    mean = mean_circle(money, (circle[0], circle[1]), circle[2], i)
+    print('mean_' + str(i), mean)
+
+    if abs(mean_cuzn - mean) < abs(mean_ni - mean):
+        cv2.circle(money, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
     else:
-        cv2.circle(money, (i[0], i[1]), i[2], (0, 0, 255), 2)
-
-    num += 1
+        cv2.circle(money, (circle[0], circle[1]), circle[2], (0, 0, 255), 2)
 
 
 cv2.imshow('money_with_circles', money)
--- Litvinov_Oleg/СТЗ/Lab1. Term2/line finder.py
+++ Litvinov_Oleg/СТЗ/Lab1. Term2/line finder.py
@@ -1,10 +1,10 @@
+import os
 import cv2
 import numpy as np
 from skeletonization import skeletonization
 from connect_lines import nothing, connectLines
 
 
-# start
 video_num = 1
 video = cv2.VideoCapture('Video/' + str(video_num) + '.avi')
 frame = video.read()[1]
@@ -14,16 +14,13 @@ shift_btw_frames = 100
 # therefore in this work will be processed frames 101, 201, ..., 501
 
 for i in range(num_of_frames):
-    print('processing frame number ', i)
+    print('\nprocessing frame number ', i)
 
     # skip frames to avoid multicollinearity
     for j in range(shift_btw_frames):
         frame = video.read()[1]
-
     cv2.imshow('original fr' + str(i) + '_video' + str(video_num), frame)
-        
-    # blur a little to prevent mistakes with binarization
-    img_blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+    
 
     # creating window with trackbars
     cv2.namedWindow('main', flags=cv2.WINDOW_AUTOSIZE)
@@ -44,14 +41,13 @@ for i in range(num_of_frames):
     rl = cv2.getTrackbarPos('rl', 'main')
     ru = cv2.getTrackbarPos('ru', 'main')
 
-
+    # blur a little to prevent mistakes with binarization
+    img_blurred = cv2.GaussianBlur(frame, (5, 5), 0)
     img_bin = np.zeros((img_blurred.shape[0], img_blurred.shape[1]), dtype=np.uint8)
-    # binarization
-    #cv2.inRange(src=orig, lowerb=(bl, gl, rl), upperb=(bu, gu, ru), dst=binar)
+    # binarize the blurred image
     cv2.inRange(src=img_blurred, lowerb=(bl, gl, rl), upperb=(bu, gu, ru), 
                 dst=img_bin)
     #cv2.imshow('binarization fr' + str(i) + '_video' + str(video_num), img_bin)
-    
 
     cv2.dilate(img_bin, (5, 5), img_bin)
     cv2.erode(img_bin, (5, 5), img_bin)
@@ -61,20 +57,26 @@ for i in range(num_of_frames):
     cv2.dilate(img_bin, (5, 5), img_bin)
     #cv2.imshow('erode and dilate fr' + str(i) + '_video' + str(video_num), img_bin)
 
-    # 255 -> 1, another -> 0
+    # prepare image format to skeletonization: 255 -> 1, another -> 0
     img_bin = img_bin // 255
 
     # Zhang-Suen algorithm
-    # these lines are commented due to the time of processing
-#    img_skeletized = skeletonization(img_bin)
-#    cv2.imwrite('Frames/skelet_'+ 'video' + str(video_num) + '_fr' + str(i) + '.png',
-#                img_skeletized)
-    
+    # load if file exists, else process
+    if os.path.isfile('Frames/skelet_'+ 'video' + str(video_num) + \
+                      '_fr' + str(i) + '.png'):
+        print('video' + str(video_num) + '_fr' + str(i) + '_exists')
+        img_skeletized = cv2.imread('Frames/skelet_' + 'video' + \
+                                    str(video_num) + '_fr' + str(i) + '.png', 
+                                    cv2.IMREAD_GRAYSCALE)
+    else:
+        print('video' + str(video_num) + '_fr' + str(i) + '_not_exists')
+        img_skeletized = skeletonization(img_bin)
+        cv2.imwrite('Frames/skelet_'+ 'video' + str(video_num) + \
+                    '_fr' + str(i) + '.png',
+                    img_skeletized)
+        
 
-    img_skeletized = cv2.imread('Frames/skelet' + 'video' + str(video_num) + '_fr' + str(i) + '.png', 
-                              cv2.IMREAD_GRAYSCALE)
-
-    # back 1 -> 255, 0 -> 0
+    # back to visible format: 1 -> 255, 0 -> 0
     img_skeletized *= 255
     cv2.imshow('skeletonization fr' + str(i) + '_video' + str(video_num), 
                img_skeletized)
@@ -82,7 +84,9 @@ for i in range(num_of_frames):
     frame_with_path = connectLines(img_skeletized, frame)
     cv2.imshow('with_path fr' + str(i) + '_video' + str(video_num), 
                frame_with_path)
+    
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
 
 cv2.waitKey(0)
